a0a103642b4f4a253fe25692c1ffefdd5a380e74
yunsin0408 <yunsin.hung@gmail.com>
2026-04-03 20:23:39 +0800

    change hash routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
 import ProjectsView from "../views/ProjectsView.vue";
@@ -6,7 +6,7 @@ import ExtracurricularView from "../views/ExtracurricularView.vue";
 import CvView from "../views/CvView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", name: "home", component: HomeView },
     { path: "/about", name: "about", component: AboutView },
